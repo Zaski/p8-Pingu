0f32d8e34738443c3540a5e17c44031c41dbf29e
Óscar Seguro <zaski123@gmail.com>
2017-01-01 15:51:04 +0100

    Added crouch animation logic. Redone sprite drawing logic

--- pingu.lua
+++ pingu.lua
@@ -15,7 +15,7 @@ COLOR_BG    = 5 -- background color
 debug   =   true
 
 -- Possible actions
-actions     = {idle=0, walk=1, jump=2}
+actions     = {idle=0, walk=1, jump=2, crouch=3}
 -- Possible movement directions
 directions  = {front= 0, right=1, left=2}
 -- Possible characters
@@ -35,14 +35,18 @@ characters  = {
         action      =   actions.idle,      -- Defalt action
         direction   =   directions.front,   -- Default direction
         moving      =   false,
-        jumping     =   true,
+        jumping     =   false,
+        crouching   =   false,
         sprites     =   {
-            idle_1      =   1,
-            idle_2      =   2,
-            walk_1      =   3,
-            walk_2      =   4,
-            jump_still  =   5,
-            jump        =   6
+            idle_1          =   1,
+            idle_2          =   2,
+            walk_1          =   3,
+            walk_2          =   4,
+            jump_still      =   5,
+            jump_side       =   6,
+            crouch_still    =   7,
+            crouch_side_1   =   8,
+            crouch_side_2   =   9
         },
     }
 }
@@ -53,6 +57,10 @@ player = characters.pingu    -- The default playable character is Pingu
 --------------------- FUNCTIONS -------------------
 
 function moveCharacter(character)
+    -- Default: character is idle
+    character.moving = false
+    character.direction = directions.front      -- Default direction is look at front
+    character.action = actions.idle             -- Default action is idle
     -- Character walks
     if btn(BR) or btn(LR) then
         character.action = actions.walk
@@ -63,10 +71,6 @@ function moveCharacter(character)
         elseif btn(LR) then
             character.direction = directions.left
         end
-    else    -- Character is idle
-        character.moving = false
-        character.direction = directions.front      -- Default direction is look at front
-        character.action = actions.idle             -- Default action is idle
     end
     -- Character jumps
     if btn(BU) then
@@ -74,6 +78,20 @@ function moveCharacter(character)
         character.jumping = true
     else
         character.jumping = false
+    end
+    -- Character crouches
+    if btn(BD) then
+        character.action = actions.crouch
+        character.crouching = true
+        if btn(BR) then
+            character.moving = true
+            character.direction = directions.right
+        elseif btn(BL) then
+            character.moving = true
+            character.direction = directions.left
+        end
+    else
+        character.crouching = false
     end
 end
 
@@ -128,23 +146,31 @@ function drawPlayer()
         palt(0, false)  -- Default transparent color is black (0), so we set as not transparent
     end
     if not player.moving then
-        if not player.jumping then
-            anim(player, player.sprites.idle_1, 2, 3, false)
-        else
+        if player.jumping then
             spr(player.sprites.jump_still, player.screen_pos.x, player.screen_pos.y)
+        elseif player.crouching then
+            spr(player.sprites.crouch_still, player.screen_pos.x, player.screen_pos.y)
+        else
+            anim(player, player.sprites.idle_1, 2, 3, false)
         end
     else
-        if not player.jumping then
+        if player.jumping then
+            if player.direction == directions.right then
+                spr(player.sprites.jump_side, player.screen_pos.x, player.screen_pos.y)
+            else
+                spr(player.sprites.jump_side, player.screen_pos.x, player.screen_pos.y, 1, 1, true)
+            end
+        elseif player.crouching then
+            if player.direction == directions.right then
+                anim(player, player.sprites.crouch_side_1, 2, 5, false)
+            else
+                anim(player, player.sprites.crouch_side_1, 2, 5, true)
+            end
+        else
             if player.direction == directions.right then
                 anim(player, player.sprites.walk_1, 2, 10, false)
             else
                 anim(player, player.sprites.walk_1, 2, 10, true)
-            end
-        else
-            if player.direction == directions.right then
-                spr(player.sprites.jump, player.screen_pos.x, player.screen_pos.y)
-            else
-                spr(player.sprites.jump, player.screen_pos.x, player.screen_pos.y, 1, 1, true)
             end
         end
     end
