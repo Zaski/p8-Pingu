3af70808ae1c4badce686bdce0294b40622f25d9
Óscar Seguro <zaski123@gmail.com>
2017-01-02 13:55:23 +0100

    Created blank gh-pages branch with just the webpage HTML and JS

--- README.md
+++
@@ -1,3 +0,0 @@
-Pingu, a Pico8 game to try out the console.
-
-The character Pingu is inspired by Tux.
--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/zsh
-/opt/picotool/p8tool build pingu.p8 --gfx pingu.gfx.p8 --sfx pingu.sfx.p8 --music pingu.music.p8 --lua pingu.lua
